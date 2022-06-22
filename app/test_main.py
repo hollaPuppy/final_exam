e81ccc5b2f0a9e139bd463699d73546cfb532eb0
hollaPuppy <roki216@mail.ru>
2022-06-22 22:31:19 +0300

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,13 +1,17 @@
 from .api import app
+import json
 from fastapi.testclient import TestClient
 
 client = TestClient(app)
 
 
-def test_admin_auth():
-    response = client.get(
-        "/users/auth",
-        json={"user_name": "Admin", "user_password": "AdminAdmin"},
-    )
+# def test_admin_auth():
+#     data = {"user_name": "Admin", "user_password": "AdminAdmin"}
+#     response = client.get('/users/auth', json.dumps(data))
+#     assert response.status_code == 200
+#     assert response.json() == {"detail": "OK"}
+
+
+def test_get_top_players():
+    response = client.get('/users/top_players/achv')
     assert response.status_code == 200
-    assert response.json() == {"detail": "OK"}
